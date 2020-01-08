0ddc177b3c37ef0b4fd8a975a61f9d7ed4563ad6
Olivier Brisse <ouranos@donarproject.org>
2020-01-09 09:38:44 +1100

    Improve Flickr service

--- src/services/FlickrFeed.js
+++ src/services/FlickrFeed.js
@@ -1,22 +1,23 @@
-// Return photos from Flickr public feed
-// https://www.flickr.com/services/feeds/photos_public.gne
-
-// tags (Optional)
-//    A comma delimited list of tags to filter the feed by.
-
 import fetchJsonp from 'fetch-jsonp';
-const PUBLIC_FEED_URL = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json';
 
-export function fetchPhotos () {
-  return fetchJsonp(PUBLIC_FEED_URL, {
+// tagmode = any -> return content if it matches any of the tags
+const PUBLIC_FEED_URL =
+  'https://api.flickr.com/services/feeds/photos_public.gne?format=json&tagmode=any';
+
+/**
+ * Return latest photos from the public Flickr feed
+ */
+export function fetchPhotos (tags = '') {
+  const endpoint =  !!tags
+    ? `${PUBLIC_FEED_URL}&tags=${tags}`
+    : `${PUBLIC_FEED_URL}`
+
+  return fetchJsonp(endpoint, {
       jsonpCallback: 'jsoncallback',
       timeout: 3000
     })
     .then((response) => response.json())
-    .then((json) => {
-      console.log(json.items);
-      return json.items
-    })
+    .then((json) => json.items)
     .catch((error) => {
       console.log("Error occurred while fetching Photos");
       console.log(error);
